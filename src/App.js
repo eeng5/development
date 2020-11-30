4397cd1f8bb538825f9dea321004549a36b28c59
Eleanor Eng <eleanor_eng@brown.edu>
2020-11-30 01:25:47 -0500

    navbars and data added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import FilteredList from './FilteredList';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
+  const stickerList = [
+    { name: "awesome", number: "one", key: 1 },
+    { name: "boba", number: "three", key: 2 },
+    { name: "cosmic", number: "one", key: 3 },
+    { name: "highland", number: "one", key: 4 },
+    { name: "mountain", number: "one", key: 5 },
+    { name: "paula", number: "one", key: 6 },
+    { name: "positivity", number: "one", key: 7 },
+    { name: "ramen", number: "one", key: 8 },
+    { name: "retro", number: "four", key: 9 },
+    { name: "rick", number: "one", key: 10 },
+    { name: "succulents", number: "twenty", key: 11 },
+    { name: "vine", number: "nine", key: 12 }
+  ]
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">ß
+        <div>
+          <FilteredList list={stickerList} />
+        </div>
       </header>
     </div>
   );
 }
 
 export default App;
+
+// contains the product list and passes it to FilteredList component
