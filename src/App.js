974ffa35bd1c44d180452c60a5920efaca7757a8
Eleanor Eng <eleanor_eng@brown.edu>
2020-11-30 23:57:10 -0500

    still need to fix price sorting

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// contains the product list and passes it to FilteredList component
 import './App.css';
 import FilteredList from './FilteredList';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -5,24 +6,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
 
   const stickerList = [
-    { name: "awesome", number: "one", key: 1, content: "music", price: "1.99" },
-    { name: "boba", number: "three", key: 2, content: "food", price: "2.99" },
-    { name: "cosmic", number: "one", key: 3, content: "music", price: "1.59" },
-    { name: "highland", number: "one", key: 4, content: "animals", price: "1.99" },
-    { name: "mountain", number: "one", key: 5 , content: "nature", price: "1.39" },
-    { name: "paula", number: "one", key: 6, content: "random", price: "1.99" },
-    { name: "positivity", number: "one", key: 7, content: "random", price: "1.29" },
-    { name: "ramen", number: "one", key: 8, content: "food", price: "1.89" },
-    { name: "retro", number: "four", key: 9, content: "random", price: "3.99" },
-    { name: "rick", number: "one", key: 10, content: "media", price: "1.99" },
-    { name: "succulents", number: "twenty", key: 11, content: "nature", price: "5.99" },
-    { name: "vine", number: "nine", key: 12, content: "media", price: "4.99" }
+    { price: "1.99", name: "Awesome Mix", number: "1", key: 1, content: "music", image: "https://ih1.redbubble.net/image.378193196.5998/st,small,507x507-pad,600x600,f8f8f8.u1.jpg" },
+    { price: "2.99", name: "Bubble Tea", number: "3", key: 2, content: "food", image: "https://ih1.redbubble.net/image.190422368.9890/st,small,507x507-pad,600x600,f8f8f8.jpg" },
+    { price: "1.59", name: "Cosmic Record", number: "1", key: 3, content: "music", image: "https://ih1.redbubble.net/image.817431979.4277/st,small,507x507-pad,600x600,f8f8f8.jpg" },
+    { price: "1.99", name: "Highland Coo", number: "1", key: 4, content: "animals", image: "https://ih1.redbubble.net/image.488065581.0607/st,small,507x507-pad,600x600,f8f8f8.u3.jpg" },
+    { price: "1.39", name: "Landscape", number: "1", key: 5 , content: "nature", image: "https://ih1.redbubble.net/image.151193938.5041/st,small,507x507-pad,600x600,f8f8f8.jpg" },
+    { price: "1.99", name: "Paula Deen", number: "1", key: 6, content: "random", image: "https://ih1.redbubble.net/image.213639840.6728/st,small,507x507-pad,600x600,f8f8f8.u2.jpg" },
+    { price: "1.29", name: "Positivity", number: "1", key: 7, content: "random", image: "https://ih1.redbubble.net/image.1612432275.6013/st,small,507x507-pad,600x600,f8f8f8.u3.jpg" },
+    { price: "1.89", name: "Ramen", number: "1", key: 8, content: "food", image: "https://ih1.redbubble.net/image.513535564.4970/st,small,507x507-pad,600x600,f8f8f8.u4.jpg" },
+    { price: "3.99", name: "Retro", number: "4", key: 9, content: "random", image: "https://ih1.redbubble.net/image.459573568.9865/st,small,507x507-pad,600x600,f8f8f8.u3.jpg" },
+    { price: "1.99", name: "Rickrolling", number: "1", key: 10, content: "media", image: "https://ih1.redbubble.net/image.680238576.6017/st,small,507x507-pad,600x600,f8f8f8.jpg" },
+    { price: "5.99", name: "Succulents", number: "20", key: 11, content: "nature", image: "https://ih1.redbubble.net/image.265257553.4872/st,small,507x507-pad,600x600,f8f8f8.jpg" },
+    { price: "4.99", name: "Vine References", number: "9", key: 12, content: "media", image: "https://ih1.redbubble.net/image.690889615.3587/st,small,507x507-pad,600x600,f8f8f8.u2.jpg" }
   ]
 
 
   return (
     <div className="App">
         <div>
+          <h1 style={{ marginTop: '1em', color: "white" }}>The Savvy Sticker Supply</h1>
           <FilteredList list={stickerList} />
         </div>
     </div>
@@ -30,5 +32,3 @@ function App() {
 }
 
 export default App;
-
-// contains the product list and passes it to FilteredList component
